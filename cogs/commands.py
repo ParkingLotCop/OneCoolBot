cdd0009b28ae7aea87211d6e67a8679c5cbfb5ea
timothypidashev <pidashev.tim@gmail.com>
2021-07-26 20:56:54 +0000

    add user-info command

--- cogs/commands.py
+++ cogs/commands.py
@@ -229,5 +229,48 @@ class Commands(commands.Cog):
 
         await context.send(embed=info)
 
+
+    #USER-INFO
+    @cog_ext.cog_slash(
+        name="user-info",
+        description="Gets user info and other handy information.",
+        guild_ids=guild_ids
+    )
+    async def userinfo(self, context: SlashContext, user: discord.Member=None):
+        await log.slash_command(self, context)
+
+        if isinstance(context.channel, discord.DMChannel):
+            return
+
+        if user is None:
+            user = context.author 
+
+        embed = discord.Embed(
+            colour=0x9b59b6
+        )
+        embed.set_author(
+            name=str(user), 
+            icon_url=user.avatar_url
+        )
+        
+        perm_string = ', '.join([str(p[0]).replace("_", " ").title() for p in user.guild_permissions if p[1]])
+        members = sorted(context.guild.members, key=lambda m: m.joined_at)
+        date_format = "%a, %d %b %Y at %I:%M %p"
+
+        top_role = user.top_role
+        
+        fields = [("Joined this server at", user.joined_at.strftime(date_format), True),
+                  ("Registered this account at", user.created_at.strftime(date_format), False),
+                  ("Server join position", str(members.index(user)+1), True),
+                  ("Roles [{}]".format(len(user.roles)-1), top_role.mention, True)]
+        
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+
+        embed.set_footer(
+            text="ID: " + str(user.id)
+        )
+        await context.send(embed=embed)
+
 def setup(client):
     client.add_cog(Commands(client))
